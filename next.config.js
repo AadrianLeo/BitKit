b94ff82ba68ba654b1f9d8ddbf763edc469d1d5c
thecoderpunk <ravisingh02082001@gmail.com>
2022-07-30 12:58:05 +0530

    Bitkit first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.dribbble.com',
+    'lh3.googleusercontent.com'
+  ]
+  }
 }
 
 module.exports = nextConfig
